ee7c680f8e248a4aa244e9f8c7c45399082adc1d
maribal <maribal.zambrano@unet.edu.ve>
2018-05-10 22:25:26 -0300

    footer

--- resources/views/pages/template/footer.blade.php
+++ resources/views/pages/template/footer.blade.php
@@ -1,30 +1,22 @@
 
 
 <footer class="page-footer " >
-
-
-
-    <div class="contenedor" style="padding-bottom: 16px;">
+    <div class="contenedor" style="padding-bottom: 40px;">
 
         <div class="row" style="margin-bottom: 0px;">
 
-
-
-            <!--First column-->
-
             <div class="col s6 m6 l1" >
-
-                <h5 class="title" >SITEMAP</h5>
-
-                    <a href="{{route('index')}}" class="texto-footer">Home</a><br>
-                    <a href="" class="texto-footer">Productos</a><br>
-                    <a href="{{route('empresa')}}" class="texto-footer">Empresa</a><br>
-                    <a href="{{route('descarga')}}" class="texto-footer">Sectores</a><br>
-                    <a href="{{route('novedades')}}" class="texto-footer">Servicios</a><br>
+                    <a href="{{route('index')}}" class="texto-footer">Inicio</a><br>
+                    <a href="" class="texto-footer">Nosotros</a><br>
+                    <a href="{{route('empresa')}}" class="texto-footer">Productos</a><br>
+                    <a href="{{route('descarga')}}" class="texto-footer">Servicios</a><br>
             </div>
 
-            <div class="col s6 m6 l2" style="padding-top: 43px;">
-                <a href="{{route('descarga')}}" class="texto-footer">Novedades</a><br>
+            <div class="col s6 m6 l2" style="padding-right: 0px;">
+                <a href="{{route('descarga')}}" class="texto-footer">Clientes</a><br>
+                <a href="{{route('descarga')}}" class="texto-footer">Calidad</a><br>
+                <a href="{{route('descarga')}}" class="texto-footer">Solicitud de Presupuesto</a><br>
+                <a href="{{route('descarga')}}" class="texto-footer">Contacto</a><br>
 
             </div>
 
@@ -37,14 +29,13 @@
             <!--fifth column-->
 
             <div class="col s12 m12 l3 texto-footer"  >
-                <h5 class="title" style="padding-left: 32px;">AIRDIN-INDOMATIX</h5>
                 <div class="row" style="margin-bottom: 6px;" >
 
-                    <div class="col s2 ">
+                    <div class="col s2 " style="padding-top: 5px;">
                         <i class="material-icons">location_on</i>
                     </div>
                     <div class="col s10 ">
-                        <a href="https://www.google.com/maps/place/Calle+12+53,+B1884CDB+Berazategui+Oeste,+Buenos+Aires/@-34.7840544,-58.2598868,17z/data=!3m1!4b1!4m5!3m4!1s0x95a3294a74505101:0x1a1e61553dd7c90!8m2!3d-34.7840544!4d-58.2576981" class="texto-mail">{{ $direccion->descripcion }}</a>
+                        <a href="https://www.google.com/maps/place/Calle+12+53,+B1884CDB+Berazategui+Oeste,+Buenos+Aires/@-34.7840544,-58.2598868,17z/data=!3m1!4b1!4m5!3m4!1s0x95a3294a74505101:0x1a1e61553dd7c90!8m2!3d-34.7840544!4d-58.2576981" class="texto-mail">{!! $direccion->descripcion !!}</a>
                     </div>
                 </div>
                 <div class="row" style="margin-bottom: 6px;">
